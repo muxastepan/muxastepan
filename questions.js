983ca03b8865d255db306913677dec55efd64837
muxastepan <58739030+muxastepan@users.noreply.github.com>
2019-12-10 19:03:28 +0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,12 @@
+let questions = [
+    {
+        text: "Что не считается содержимым при box-sizing: border-box?",
+        options:["padding","margin","border","Всё считается содержимым"],
+        correct: 1
+    },
+    {
+        text: "Каким будет результат вычисления 1 / 0 в JavaScript?",
+        options: ["null","undefined","Infinity","Ошибка"],
+        correct: 2
+    }
+];
